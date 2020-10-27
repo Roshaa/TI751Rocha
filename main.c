224b8c255cf1274735b19ea5dfce4363507f8fe7
Ricardo Rocha <ricardo_rocha_97@hotmail.com>
2020-10-27 11:20:20 +0000

    Versao 1.1

--- main.c
+++ main.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 int main()
 {
-    printf("Hello world!\n");
+    float a,b,c,c1,r,a1;
+
+    printf("Olá eu calculo teoremas de pitágoras\n");
+    printf("Introduza o 1º cateto\n")
+    scanf("%f",&c);
+
+    printf("Introduza o 2º cateto\n")
+    scanf("%f",&c1);
+
+    a=c*c;
+    b=c1*c1;
+    a1=a+b;
+    r= sqrt(a1);
+
+    printf("A Hipotenusa mede %f",r)
+
+
     return 0;
 }
