2c3d1336866873bd710b6b667bd328092edb57ec
Ricardo Rocha <ricardo_rocha_97@hotmail.com>
2020-10-27 11:38:04 +0000

    Versao final

--- main.c
+++ main.c
@@ -4,13 +4,13 @@
 
 int main()
 {
-    float a,b,c,c1,r,a1;
+    float a,b,c,c1,r,a1,p,area;
 
-    printf("Olá eu calculo teoremas de pitágoras\n");
-    printf("Introduza o 1º cateto\n")
+    printf("Ola eu calculo teoremas de pitágoras\n");
+    printf("Introduza o primeiro cateto\n");
     scanf("%f",&c);
 
-    printf("Introduza o 2º cateto\n")
+    printf("Introduza o segundo cateto\n");
     scanf("%f",&c1);
 
     a=c*c;
@@ -18,7 +18,12 @@ int main()
     a1=a+b;
     r= sqrt(a1);
 
-    printf("A Hipotenusa mede %f",r)
+    printf("A Hipotenusa mede %.2f\n",r);
+
+    p=(c+c1+r)/2;
+    printf("O perimetro e  %.2f\n",p);
+    area=sqrt(p*(p-c)*(p-r)*(p-c1));
+    printf("A area do triangulo e %.2f\n",area);
 
 
     return 0;
